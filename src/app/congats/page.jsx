0a5e806cd1972b72376e28b0b2969952cd8dffd2
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-02-23 13:48:03 +0200

    update

--- src/app/congats/page.jsx
+++ src/app/congats/page.jsx
@@ -1,29 +1,15 @@
 'use client'
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import bg from '/public/bg.png';
 import doneImage from '/public/done.svg';
 import MazarInfo from '../../components/book/MazarInfo';
 import FormPage from '../../components/book/FormPage';
 import Image from 'next/image';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Book() {
     const router = useRouter()
-    const seachParams = useSearchParams()
-    const name = seachParams.get('name')
-    const phone = seachParams.get('phone')
-    const package_name = seachParams.get('package')
-    useEffect(() => {
-        // **Push data to GTM dataLayer**
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-            event: 'form_submission',
-            customer_name: name,
-            customer_whatsapp: phone,
-            package_name: package_name,
-        });
-    })
-
+    
     return (
         <div className="popup" >
             <div className="popup-cont">
